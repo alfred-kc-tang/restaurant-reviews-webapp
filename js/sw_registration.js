64d73fb40f0b723778ca28f1372d1ee5f96a54b1
alfred-kctang <43289883+alfred-kctang@users.noreply.github.com>
2019-03-14 17:30:24 +0800

    Update sw_registration.js

--- js/sw_registration.js
+++ js/sw_registration.js
@@ -1,7 +1,7 @@
 /* This file is for registering for service worker */
 document.addEventListener('DOMContentLoaded', event => {
     if (navigator.serviceWorker) {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('./sw.js')
         .then(reg => {
             console.log('Service worker registration succeeds: ' + reg.scope);
         })
